46118c9dc57c49e2b1f6d4e0dac935101035da4c
himanshiiiiii <99973728+himanshiiiiii@users.noreply.github.com>
2024-07-25 07:47:34 +0530

    Time: 317 ms (39.65%), Space: 142.2 MB (42.52%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -2,47 +2,44 @@ class Solution {
 public:
     void merge(vector<int>& nums,int l,int h,int mid)
     {
-        int left=l ,right=mid+1;
-        vector<int>ans;
-        while(left<=mid && right<=h)
+        int n=nums.size();
+        int left=l,right=mid+1;
+        int k=0;
+        vector<int>temp(h-l+1);
+        while(left<=mid&&right<=h)
         {
             if(nums[left]<nums[right])
-            {
-                ans.push_back(nums[left]);
-                left++;
-            }
+                temp[k++]=nums[left++];
             else
-            {
-                    ans.push_back(nums[right]);
-                right++;
-            }
-        }
-         while(left<=mid )
-        {
-             ans.push_back(nums[left]);
-                left++;
+                temp[k++]=nums[right++];
         }
          while(right<=h)
         {
-            
-             ans.push_back(nums[right]);
-                right++;
-         }
+                temp[k++]=nums[right++];
+        }
+         while(left<=mid)
+        {
+                temp[k++]=nums[left++];
+        }
         
         for(int i=l;i<=h;i++)
-        nums[i]=ans[i-l];
+        {
+            nums[i]=temp[i-l];
+        }
+        
+       
         
     }
     void mergesort(vector<int>& nums,int l,int h)
     {
-        if(l>=h)return;
         
-        int mid=l+(h-l)/2;
-        
-        mergesort(nums,l,mid);
-         mergesort(nums,mid+1,h);
-        merge(nums,l,h,mid);
-            
+     if(l<h)
+          {
+              int mid=l+(h-l)/2;
+              mergesort(nums,l,mid);
+              mergesort(nums,mid+1,h);
+              merge(nums,l,h,mid);
+          }
     }
     
     vector<int> sortArray(vector<int>& nums) {
